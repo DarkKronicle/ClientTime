48b552736a2f9c2a0a719694d6177113c0b3a031
DarkKronicle <darkkronicle@gmail.com>
2021-12-19 21:35:33 -0700

    Adding README

---
+++ README.md
@@ -0,0 +1,21 @@
+# ClientTime
+
+ClientTime is a simple fabric mod that allows for control over time. There are different ways to change and alter the time. This mod is completely client sided so it works on any server, and is purely visual.
+
+## Features
+
+- Different types of time setting
+  - Static
+  - Loop in different ways
+- Smooth speed change and rendering
+- Weather is planned!
+
+## Dependencies
+
+To run this mod [MaLiLib](https://www.curseforge.com/minecraft/mc-mods/malilib) is required! [ModMenu](https://www.curseforge.com/minecraft/mc-mods/modmenu) is needed for configuration.
+
+## Credits
+
+Code & Mastermind: DarkKronicle
+
+Proofreading & Language: Chronos22
--- src/main/java/io/github/darkkronicle/clienttime/ClientTime.java
+++ src/main/java/io/github/darkkronicle/clienttime/ClientTime.java
@@ -1,16 +1,9 @@
 package io.github.darkkronicle.clienttime;
 
 import fi.dy.masa.malilib.event.InitializationHandler;
-import fi.dy.masa.malilib.gui.GuiBase;
-import io.github.darkkronicle.clienttime.config.TimeConfigScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class ClientTime implements ClientModInitializer {
@@ -19,20 +12,6 @@ public class ClientTime implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        KeyBinding keyBinding =
-                new KeyBinding(
-                        "clienttime.key.opensettings",
-                        InputUtil.Type.KEYSYM,
-                        GLFW.GLFW_KEY_Y,
-                        "clienttime.category.keys");
-        KeyBindingHelper.registerKeyBinding(keyBinding);
-        ClientTickEvents.START_CLIENT_TICK.register(
-                s -> {
-                    if (keyBinding.wasPressed()) {
-                        GuiBase.openGui(new TimeConfigScreen());
-                    }
-                }
-        );
         InitializationHandler.getInstance().registerInitializationHandler(new ClientTimeInitHandler());
     }
 
